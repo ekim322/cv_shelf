ece62f8d584baa739b61b73e8e465fc9d5466d5a
ekim322 <evankim9903@gmail.com>
2022-07-06 12:19:07 -0700

    removed softmax

--- TripletNet/model.py
+++ TripletNet/model.py
@@ -4,20 +4,19 @@ import torch.nn as nn
 from torchvision.models import resnet50
 
 class TripletNet(nn.Module):
-    def __init__(self, n_classes=81):
+    def __init__(self, embedding_size=81):
         super(TripletNet, self).__init__()
         resnet = resnet50(pretrained=True)
         for param in resnet.parameters():
             param.requires_grad = True
-        resnet.fc = nn.Linear(2048, n_classes)
+        resnet.fc = nn.Linear(2048, embedding_size)
         
         self.resnet = resnet
-        self.sfmx = nn.Softmax(dim=1)
 
     def forward(self, anc_img, pos_img, neg_img):
-        anc_pred = self.sfmx(self.resnet(anc_img))
-        pos_pred = self.sfmx(self.resnet(pos_img))
-        neg_pred = self.sfmx(self.resnet(neg_img))
+        anc_pred = self.resnet(anc_img)
+        pos_pred = self.resnet(pos_img)
+        neg_pred = self.resnet(neg_img)
 
         return anc_pred, pos_pred, neg_pred 
 
